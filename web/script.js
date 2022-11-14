c868ba03e64bee04593108094965f975f07eab58
João Lucas A. Durigan <lucasdurigan@hotmail.com>
2022-11-13 21:49:09 -0300

    retirado as referências de python

--- web/script.js
+++ web/script.js
@@ -1,17 +1,1 @@
 // Onclick of the button
-
-
-function retificadorEmPonte(){
-  var voltagem1 = 220;
-  var frequencia1 = 60;
-  var capacitor = 0.0001;
-  var resistencia = 1000;
-  var N1 = 18000;
-  var N2 = 1200;
-  diodos = 0.7;
-  eel.retificadorEmPonte()(function(voltagem1, frequencia1, capacitor, resistencia, N1, N2, diodos){})
-}
-
-$("button").click(function(){
-  retificadorEmPonte();
-});
