49237e996ef9c8d959553c66f99f21b5c91d5534
Gabriel Nunes <gabrielchagasn@gmail.com>
2022-11-15 19:56:59 -0300

    Subindo nova versão do calculo em ponte

--- web/retificadorEmPonte.js
+++ web/retificadorEmPonte.js
@@ -1,41 +1,41 @@
+// RETIFICADOR ONDA COMPLETA EM PONTE
 
-//Circuito com retificador em ponte
-
-let voltagem1 = parseFloat(200)
+let voltagem1 = parseFloat(220)
 let frequencia1 = parseFloat(60)
-let capacitor = parseFloat(0.0001)
 let resistencia = parseFloat(1000)
-let N1 = parseFloat(1000)
-let N2 = parseFloat(200)
+let N1 = parseFloat(18000)
+let N2 = parseFloat(1200)
 let diodos = parseFloat(0.7)
 
-function voltagemSecundario() {
-    return Math.round((voltagem1 * N2) / N1, 2)
+      
+function voltagemSecundario(){
+  return ((voltagem1*N2)/N1).toFixed(2)
 }
-function vp() {
-    return Math.round(Math.sqrt(2) * voltagemSecundario(), 2)
-}
-function VRLP() {
-    return Math.round(vp() - (2 * diodos), 2)
-}
-function corrente() {
-    return Math.round(VRLP() / resistencia, 2)
-}
-function ripple() {
-    return Math.round(corrente() / (capacitor * (2 * frequencia1)), 2)
-}
-function tensaoMedia() {
-    return Math.round((VRLP() - ripple()), 2)
-}
-function eficaz() {
-    return Math.round(VRLP() / Math.sqrt(2), 2)
+      
+function vp(){
+ return (Math.sqrt(2)*voltagemSecundario()).toFixed(2)
 }
 
-console.log("Tensão eficaz no V2 = ", voltagemSecundario())
-console.log("Tensão de pico do V2 = ", vp())
-console.log("Tensão de pico na carga = ", VRLP())
-console.log("Tensão média na carga = ", tensaoMedia())
-console.log("Tensão eficaz na carga = ", eficaz())
-console.log("Tensão de pico no capacitor = ", VRLP())
-console.log("Corrente = ", corrente())
-console.log("Tensão de Ripple = ", ripple())
+function VRLP(){
+ return (vp() - (2*diodos)).toFixed(2)
+}  
+
+function VRLMed(){
+  return ((2*VRLP())/Math.PI).toFixed(2)
+}
+
+function eficaz(){
+  return (VRLP()/Math.sqrt(2)).toFixed(2)
+}
+
+function freqCarga(){
+  return (2*frequencia1).toFixed(2)
+}
+
+console.log("Tensão eficaz no V2 = ", voltagemSecundario(),"v")
+console.log("Tensão de pico do V2 = ", vp(),"v")
+console.log("Tensão de pico na carga = ", VRLP(),"v")
+console.log("Tensão média na carga = ", VRLMed(),"v")
+console.log("Tensão eficaz na carga = ", eficaz(),"v")
+console.log("Frequência na carda = ", freqCarga(), "Hz")
+console.log("Tensão de pico inversa no diodo = ", vp(),"v")
