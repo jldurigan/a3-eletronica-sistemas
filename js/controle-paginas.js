c8cb96dbf13159aaec7041af336ae1a11d03bbfb
João Lucas A. Durigan <lucasdurigan@hotmail.com>
2022-11-21 01:19:05 -0300

    arquivo _Layout.html alterado novamente para index.html
    implementado a function para carregamento de conteúdo no arquivo index

--- js/controle-paginas.js
+++ js/controle-paginas.js
@@ -1,9 +1,3 @@
-$(document).ready(function () {
-
-    $("a").click(function (event) {
-        alert("alo");
-        event.preventDefault();
-        var page = $(this).attr('href');
-        $('#conteudo').load(page);
-    });
-});
+function carregarConteudo(page){
+    $('#conteudo').load(page);
+}
