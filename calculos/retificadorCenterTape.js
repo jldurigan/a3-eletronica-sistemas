317c95f592c89b4adeeb1b5d8fe662e0772adfaf
stephuz <stephanienpaulino@gmail.com>
2022-12-04 16:03:58 -0300

    atualização geral

--- calculos/retificadorCenterTape.js
+++ calculos/retificadorCenterTape.js
@@ -1,13 +1,19 @@
-let voltagem1 = parseFloat(200)
-let frequencia1 = parseFloat(60)
-let capacitor = parseFloat(0.0001)
-let resistencia = parseFloat(4000)
-let N1 = parseFloat(1000)
-let N2 = parseFloat(200)
+
+let btnPonte = document.getElementById("BotaoCalcular")
+let btnLimpar = document.getElementById("BotaoReset")
+
+
+btnPonte.addEventListener("click", function (event) {
+let voltagem1 = parseFloat(document.getElementById("voltagem").value);
+let frequencia1 = parseFloat(document.getElementById("frequencia").value)
+let capacitor = parseFloat(document.getElementById("capacitor").value)
+let resistencia = parseFloat(document.getElementById("resistencia").value)
+let N1 = parseFloat(document.getElementById("n1").value)
+let N2 = parseFloat(document.getElementById("n2").value)
 let diodo = parseFloat(0.7)
 
 function voltagemSecundario() {
-    return (voltagem1*N2)/N1
+    return ((voltagem1*N2)/N1).toFixed
 }
 function picoSecundario() {
     return (Math.sqrt(2) * voltagemSecundario()).toFixed(2)
@@ -17,15 +23,57 @@ function picoCapacitor(){
 }
 
 function ripple() {
-    return ((picoCapacitor()/resistencia) / (capacitor * 2 * frequencia1)).toFixed(2)
+    return ((picoCapacitor()/resistencia) / ((capacitor*0.000001) * 2 * frequencia1)).toFixed(2)
 }
 function tensaoMedia() {
     return ((picoCapacitor()+(picoCapacitor()-ripple()))/2).toFixed(2)
 }
 
+var resTensaoV2 = document.getElementById("tensaoV2")
+    var resTensaoPicoV2 = document.getElementById("tensaoPicoV2")
+    var resTensaoPicoCapacitor = document.getElementById("tensaoPicoCapacitor")
+    var resTensaoRipple = document.getElementById("tensaoRipple")
+    var resTensaoMediaCarga = document.getElementById("tensaoMediaCarga")
 
+    resTensaoV2.textContent = "Tensão eficaz no V2 = " + voltagemSecundario().replace(".", ",") + "v"
+    resTensaoPicoV2.textContent = "Tensão de pico do V2 = " + picoSecundario().replace(".", ",") + "v"
+    resTensaoPicoCapacitor.textContent = "Tensão de pico no capacitor = " + picoCapacitor().toFixed(2).replace(".", ",") + "v"
+    resTensaoRipple.textContent = "Tensão de Ripple = " + ripple().replace(".", ",") + "v"
+    resTensaoMediaCarga.textContent = "Tensão média na carga = " + tensaoMedia().replace(".", ",") + "v"
+})
+
+btnLimpar.addEventListener("click", function(event){
+    var valorVoltagem = document.getElementById("voltagem")
+    var valorFrequencia = document.getElementById("frequencia")
+    var valorCapacitor = document.getElementById("capacitor")
+    var valorResistencia = document.getElementById("resistencia")
+    var valorN1 = document.getElementById("n1")
+    var valorN2 = document.getElementById("n2")
+
+    var resTensaoV2 = document.getElementById("tensaoV2")
+    var resTensaoPicoV2 = document.getElementById("tensaoPicoV2")
+    var resTensaoPicoCapacitor = document.getElementById("tensaoPicoCapacitor")
+    var resTensaoRipple = document.getElementById("tensaoRipple")
+    var resTensaoMediaCarga = document.getElementById("tensaoMediaCarga")
+
+    valorVoltagem.value = ""
+    valorFrequencia.value = ""
+    valorCapacitor.value = ""
+    valorResistencia.value = ""
+    valorN1.value = ""
+    valorN2.value = ""
+
+    resTensaoV2.textContent = "Tensão eficaz no V2 = "
+    resTensaoPicoV2.textContent = "Tensão de pico do V2 = "
+    resTensaoPicoCapacitor.textContent = "Tensão de pico no capacitor = "
+    resTensaoRipple.textContent = "Tensão de Ripple = "
+    resTensaoMediaCarga.textContent = "Tensão média na carga = "
+})  
+
+/*
 console.log("Tensão eficaz no V2 = ", voltagemSecundario()+"v")
 console.log("Tensão de pico do V2 = ", picoSecundario()+"v")
 console.log("Tensão de pico no capacitor = ", picoCapacitor()+"v")
 console.log("Tensão de Ripple = ", ripple()+"v")
 console.log("Tensão média na carga = ", tensaoMedia()+"v")
+*/
--- calculos/retificadorMeiaOnda.js
+++ calculos/retificadorMeiaOnda.js
@@ -1,31 +1,79 @@
 //Circuito com retificador de meia onda
 
-let volt1 = parseFloat(127)
-let N1 = parseFloat(10)
-let N2 = parseFloat(1)
-let diodo = parseFloat(0.7)
-let frequencia = parseFloat(60)
-let resistencia = parseFloat(1000)
-let capacitor = parseFloat(0.0001)
+let btnPonte = document.getElementById("BotaoCalcular")
+let btnLimpar = document.getElementById("BotaoReset")
 
-function voltagemSecundario() {
-    return (volt1 * N2) / N1
-}
-function picoSecundario() {
-    return (voltagemSecundario()*Math.sqrt(2)).toFixed(2)
-}
-function picoCapacitor() {
-    return picoSecundario() - diodo
-}
-function ripple() {
-    return ((picoCapacitor()/resistencia) / (capacitor * frequencia)).toFixed(2)
-}
-function tensaoMedia() {
-    return ((picoCapacitor()+(picoCapacitor()-ripple()))/2).toFixed(2)
-}
+btnPonte.addEventListener("click", function (event) {
 
+    let voltagem1 = parseFloat(document.getElementById("voltagem").value);
+    let frequencia1 = parseFloat(document.getElementById("frequencia").value)
+    let capacitor = parseFloat(document.getElementById("capacitor").value)
+    let resistencia = parseFloat(document.getElementById("resistencia").value)
+    let N1 = parseFloat(document.getElementById("n1").value)
+    let N2 = parseFloat(document.getElementById("n2").value)
+    let diodo = 0.7
+
+    function voltagemSecundario() {
+        return ((voltagem1 * N2) / N1).toFixed(2)
+    }
+    function picoSecundario() {
+        return (voltagemSecundario() * Math.sqrt(2)).toFixed(2)
+    }
+    function picoCapacitor() {
+        return picoSecundario() - diodo
+    }
+    function ripple() {
+        return ((picoCapacitor() / resistencia) / ((capacitor*0.000001) * frequencia1)).toFixed(2)
+    }
+    function tensaoMedia() {
+        return ((picoCapacitor() + (picoCapacitor() - ripple())) / 2).toFixed(2)
+    }
+
+    var resTensaoV2 = document.getElementById("tensaoV2")
+    var resTensaoPicoV2 = document.getElementById("tensaoPicoV2")
+    var resTensaoPicoCapacitor = document.getElementById("tensaoPicoCapacitor")
+    var resTensaoRipple = document.getElementById("tensaoRipple")
+    var resTensaoMediaCarga = document.getElementById("tensaoMediaCarga")
+
+    resTensaoV2.textContent = "Tensão eficaz no V2 = " + voltagemSecundario().replace(".", ",") + "v"
+    resTensaoPicoV2.textContent = "Tensão de pico do V2 = " + picoSecundario().replace(".", ",") + "v"
+    resTensaoPicoCapacitor.textContent = "Tensão de pico no capacitor = " + picoCapacitor().toFixed(2).replace(".", ",") + "v"
+    resTensaoRipple.textContent = "Tensão de Ripple = " + ripple().replace(".", ",") + "v"
+    resTensaoMediaCarga.textContent = "Tensão média na carga = " + tensaoMedia().replace(".", ",") + "v"
+})
+
+btnLimpar.addEventListener("click", function(event){
+    var valorVoltagem = document.getElementById("voltagem")
+    var valorFrequencia = document.getElementById("frequencia")
+    var valorCapacitor = document.getElementById("capacitor")
+    var valorResistencia = document.getElementById("resistencia")
+    var valorN1 = document.getElementById("n1")
+    var valorN2 = document.getElementById("n2")
+
+    var resTensaoV2 = document.getElementById("tensaoV2")
+    var resTensaoPicoV2 = document.getElementById("tensaoPicoV2")
+    var resTensaoPicoCapacitor = document.getElementById("tensaoPicoCapacitor")
+    var resTensaoRipple = document.getElementById("tensaoRipple")
+    var resTensaoMediaCarga = document.getElementById("tensaoMediaCarga")
+
+    valorVoltagem.value = ""
+    valorFrequencia.value = ""
+    valorCapacitor.value = ""
+    valorResistencia.value = ""
+    valorN1.value = ""
+    valorN2.value = ""
+
+    resTensaoV2.textContent = "Tensão eficaz no V2 = "
+    resTensaoPicoV2.textContent = "Tensão de pico do V2 = "
+    resTensaoPicoCapacitor.textContent = "Tensão de pico no capacitor = "
+    resTensaoRipple.textContent = "Tensão de Ripple = "
+    resTensaoMediaCarga.textContent = "Tensão média na carga = "
+})
+
+/*
 console.log("Tensão eficaz no V2 = ", voltagemSecundario()+"v")
 console.log("Tensão de pico do V2 = ", picoSecundario()+"v")
 console.log("Tensão de pico no capacitor = ", picoCapacitor()+"v")
 console.log("Tensão de Ripple = ", ripple()+"v")
 console.log("Tensão média na carga = ", tensaoMedia()+"v")
+*/
